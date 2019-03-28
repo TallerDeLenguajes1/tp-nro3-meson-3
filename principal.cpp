8f5174ace33471bdfb07e7819e38872dd6b08ee4
IsabellaMuroni <isa_sk_hotmail.com>
2019-03-28 19:39:23 -0300

    Grupo 1

--- principal.cpp
+++ principal.cpp
@@ -2,26 +2,35 @@
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
+
+
 enum TRaza{Orco, Humano, Mago, Enano, Elfo};
 char Nombres[6][10];
 char Apellidos[6][10];
+
 struct TDatos {
-TRaza Raza;
-Char *ApellidoNombre;
-int edad; 
-double Salud;
+
+	TRaza Raza;
+	Char *ApellidoNombre;
+	int edad; 
+	double Salud;
 };
+
 struct TCaracteristicas{
-int velocidad;
-int destreza; 
-int fuerza;
-int Nivel; 
-int Armadura; 
+	int velocidad;
+	int destreza; 
+	int fuerza;
+	int Nivel; 
+	int Armadura; 
 };
+
 struct TPersonaje {
-TDatos * DatosPersonales;
-TCaracteristicas * Caracteristicas;
+	TDatos * DatosPersonales;
+	TCaracteristicas * Caracteristicas;
 };
 
 void main()
-{}
+{
+	//Es el grupo uno
+	nbjnbfjbu
+}
