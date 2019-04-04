af37fa8f850b60a642438a0aac44bdb1dbb2ca59
Narutou50 <negro.diaz.27@gmail.com>
2019-04-04 18:25:33 -0300

    Tarea01

--- principal.cpp
+++ principal.cpp
@@ -6,11 +6,12 @@ enum TRaza{Orco, Humano, Mago, Enano, Elfo};
 char Nombres[6][10];
 char Apellidos[6][10];
 struct TDatos {
-TRaza Raza;
-Char *ApellidoNombre;
-int edad; 
-double Salud;
+	TRaza Raza;
+	char *ApellidoNombre;
+	int edad; 
+	double Salud;
 };
+typedef struct TDatos TD;
 struct TCaracteristicas{
 int velocidad;
 int destreza; 
@@ -18,10 +19,46 @@ int fuerza;
 int Nivel; 
 int Armadura; 
 };
+typedef struct TCarcteristicas TC;
 struct TPersonaje {
 TDatos * DatosPersonales;
 TCaracteristicas * Caracteristicas;
 };
+typedef struct TPersonaje TP;
 
-void main()
-{}
+void CargarCaracteristicas(TCaracteristicas *puntero);
+void MostrarCaracteristicas(TCaracteristicas *puntero);
+
+
+void main(){	
+	TCaracteristicas *ptr = (TCaracteristicas *) malloc(sizeof(TCaracteristicas));
+	CargarCaracteristicas(ptr);
+	MostrarCaracteristicas(ptr);
+
+
+
+};
+
+void CargarCaracteristicas(TCaracteristicas *puntero){
+	printf("Caracteristicas del personaje: \n");
+	printf("velocidad: \n");
+	scanf("%d",&puntero -> velocidad);
+	printf("destreza: \n");
+	scanf("%d",&puntero -> destreza);
+	printf("fuerza: \n");
+	scanf("%d",&puntero -> fuerza);
+	printf("Nivel: \n");
+	scanf("%d",&puntero -> Nivel);
+	printf("Armadura: \n");
+	scanf("%d",&puntero -> Armadura);
+
+}
+
+void MostrarCaracteristicas(TCaracteristicas *puntero){
+	printf("Introducir Caracteristicas del personaje: \n");
+	printf("velocidad: %d\n", puntero->velocidad);
+	printf("destreza: %d\n", puntero->destreza);
+	printf("fuerza: %d\n", puntero->fuerza);
+	printf("Nivel: %d\n", puntero->Nivel);
+	printf("Armadura: %d\n", puntero->Armadura);
+}
